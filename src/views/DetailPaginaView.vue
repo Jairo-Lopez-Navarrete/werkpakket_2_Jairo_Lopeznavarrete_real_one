d491c55ab6a8665870c8fe62d5b4d9eab186e9eb
JairoLopeznavarretePXL <jairo.lopeznavarrete@student.pxl.be>
2023-12-03 17:22:18 +0100

    hard coded weg doen5

--- src/views/DetailPaginaView.vue
+++ src/views/DetailPaginaView.vue
@@ -11,17 +11,17 @@
         <img class="image" src="@/assets/img_5.png" alt="gitaar3">
       </div>
       <div class="picture-item">
-        <h1 class="card-overlay-heading">Ibanez 3500X (small)</h1>
+        <h1 class="card-overlay-heading">{{producten.titel}}</h1> <!--Ibanez 3500X (small)-->
         <img src="@/assets/guitar.png" alt="guitar1" class="card-image">
       </div>
       <div class="text-number">
         <p><b>{{producten.titel}}</b>{{producten.omschrijving}}</p>
-        <p class="price">499.99 euro</p>
+        <p class="price">{{producten.price}}</p> <!--499.99 euro-->
         <form>
           <input id="hoeveelheid" type="number">
-          <label for="hoeveelheid">Hoeveelheid</label>
+          <label for="hoeveelheid">{{ hoeveelHeid }}</label>
         </form>
-        <button class="buybutton">Koop product</button>
+        <button class="buybutton">{{buyProduct}}</button>
       </div>
     </div>
   </header>
@@ -31,8 +31,25 @@
 
 <script>
 import NavComponent from "@/components/NavComponent.vue";
+import producten from '@/json/producten.json';
 export default {
   name: "DetailPaginaView",
+
+  data(){
+    return{
+      data: producten
+    }
+  },
+
+  computed: {
+    hoeveelHeid(){
+      return "Hoeveelheid"
+    },
+    buyProduct(){
+      return "Koop product!"
+    }
+  },
+
   components: {
     NavComponent
   }
