30d89e4313627a126da7625df61c1b6bb19a06ad
JairoLopeznavarretePXL <jairo.lopeznavarrete@student.pxl.be>
2023-12-06 01:34:40 +0100

    22

--- src/components/PopularProductsComponent.vue
+++ src/components/PopularProductsComponent.vue
@@ -6,9 +6,8 @@
           <div class="card-overlay"> <!--Dit kan de reden zijn waarom mijn code niet meer werkte v-for="item in data" :key="item.id"-->
             <h1 class="card-overlay-heading">{{item.titel}}</h1>
             <p class="card-overlay-paragraph">{{prijsText}}{{item.price}}</p>
-            <router-link to="'/detail' + item.id" class='link-decoration'>
-            <button class="card-overlay-button">{{buttonText}}</button>
-            </router-link><!--v-bind toepassen op de popularproduct zelf om zo naar de detailpagina te gaan-->
+            <button class="card-overlay-button"><router-link to="'/detail' + item.id" class='link-decoration'>{{buttonText}}</router-link></button> <!--v-bind toepassen op de popularproduct zelf om zo naar de detailpagina te gaan-->
+            <router-view/>
           </div>
           <img :src="'src/assets/' + item.afbeelding" :alt="item.titel" class="card-image">
           <!--<img :src="'@/assets/${item.afbeelding}'" :alt="item.titel" class="card-image">--> <!-- dit is foute code maar werkt om pagina te zien src="@/assets/guitar.png" alt="gitaren"--> <!--werkt niet :src="'@/assets/${item.afbeelding}'" :alt="item.id"-->
