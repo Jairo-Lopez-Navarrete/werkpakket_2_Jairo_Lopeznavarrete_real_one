f78cedafb7772c17c1fac6e3040187872bd91056
JairoLopeznavarretePXL <jairo.lopeznavarrete@student.pxl.be>
2023-12-20 16:10:43 +0100

    7

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -34,22 +34,6 @@ export default {
     merkNaam() {
       return "Strings Of Fire"
     },
-    headerPartOne(){
-      return "Welkom bij Strings of Fire, jouw online bestemming voor gitaren en muziekaccessoires. Wij zijn toegewijd\n" +
-        "        aan het leveren van hoogwaardige gitaren en deskundige begeleiding, of je nu een beginner bent of een\n" +
-        "        ervaren muzikant. Ontdek onze selectie en laat de muziek beginnen!"
-    },
-    headerPartTwo(){
-      return "Onze online winkel is ontworpen met jouw muzikale reis in gedachten. We bieden een zorgvuldig samengestelde\n" +
-        "        selectie van gitaren, variërend van beginnersmodellen tot professionele instrumenten, evenals accessoires\n" +
-        "        die je helpen jouw geluid te vormen. Ons team van gepassioneerde gitaristen staat klaar om hun kennis te\n" +
-        "        delen en je te helpen bij het maken van de juiste keuzes."
-    },
-    headerPartThree(){
-      return "Dank je wel dat je Strings Of Fire hebt gekozen als jouw partner in muziek. Samen willen we jouw muzikale\n" +
-        "        visie realiseren en een blijvende verbinding opbouwen met onze gemeenschap van muzikanten. Ontdek onze\n" +
-        "        collectie en laat de muziek samenkomen!"
-    }
   }
 }
 
