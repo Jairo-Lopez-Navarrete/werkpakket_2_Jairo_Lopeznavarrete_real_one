df3b2ebd529ec18b6c489128a98cc8fed2a9b506
JairoLopeznavarretePXL <jairo.lopeznavarrete@student.pxl.be>
2023-12-21 22:34:18 +0100

    alles werkt7

--- src/stores/user.js
+++ src/stores/user.js
@@ -15,7 +15,6 @@ export const useUserStore = defineStore('user', {
         this.currentUser = user;
         this.isLoggedIn = true;
         this.token = 'yourGeneratedToken';
-        document.cookie = `authToken=${this.token};path=/;max-age=86400`;
         return true;
       } else {
         return false;
@@ -25,7 +24,6 @@ export const useUserStore = defineStore('user', {
       this.currentUser = null;
       this.isLoggedIn = false;
       this.token = null;
-      document.cookie = 'authToken=;path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT';
     },
   },
 });
